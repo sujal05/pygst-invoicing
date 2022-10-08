281d337c55aa717d6e70e97c81150d445d7747e8
Sujal Jain <sujaljain@skiff.com>
2022-10-09 01:07:08 +0530

    working prototype -added funcs for prod,client....

--- gst-billing-system-12std.py
+++ gst-billing-system-12std.py
@@ -1,81 +1,77 @@
-import mysql.connector
+import mysql.connector, decimal
 
 connection = mysql.connector.connect(host='localhost',database='gstin',user='sujal',password='sujal')
 
-cursor = connection.cursor()
-
-# try:
-#     sample_query = '''create table if not exists customers
-#     (inv int primary key auto_increment, party varchar(100) not null, date date not null, address varchar(400) not null, mobile bigint(11))'''
-
-#     cursor = connection.cursor()
-#     result = cursor.execute(sample_query)
-#     print("Sample test succeeded! fine")
-
-# except mysql.connector.Error as error:
-#     print("Failed to create table in MySQL: {}".format(error))
-# finally:
-#     if connection.is_connected():
-#         cursor.close()
-#         connection.close()
-#         print("MySQL now disconnected")
-
+cursor = connection.cursor(buffered=True)
 
 try:
-    create_table = '''create table if not exists products
-    (inv int primary key auto_increment unsigned, prod varchar(225) not null default 'Plug', model varchar(255) default 'N/A', serial varchar(255) unique default 'N/A', qty int(6) not null unsigned default 1, price decimal(11,2) not null unsigned default 0, calcrate decimal(4,2) unsigned not null default 0, amount decimal(12,2) unsigned not null default 0)'''
+    c_table = '''create table if not exists customers(inv int primary key auto_increment, party varchar(100) not null, date date not null, address varchar(400) not null, mobile bigint(11) unsigned zerofill)'''
     
-    cursor.execute(create_table)
-    print("Table creation successful.")
+    p_table = '''create table if not exists products
+    (product_id int unsigned primary key auto_increment,inv int unsigned not null, prod varchar(225) not null default 'Plug', model varchar(255) default 'N/A', serial varchar(255) default 'N/A', qty int(6) unsigned not null default 1, price decimal(11,2) unsigned not null default 0, calcrate decimal(4,2) unsigned not null default 0, amount decimal(12,2) unsigned not null default 0)'''
+
+    
+    cursor.execute(c_table)
+    cursor.execute(p_table)
 
 except mysql.connector.Error as error:
     print("Failed to create table in MySQL: {}".format(error))
 
+def clients():
+    # inv = int(input("Invoice No: "))
+    party = input("Party Name: ")
+    date = input("Date of Invoice: ")
+    address = input("Client Address: ")
+    mobile = int(input("Client Mobile: "))
+    c_query = '''insert into customers (party,date,address,mobile) values('{}','{}','{}',{})'''.format(party,date,address,mobile)
+    cursor.execute(c_query)
+    
+    connection.commit()
+    
+    print(cursor.rowcount, "record(s) inserted.\n")
+    cursor.execute("SELECT inv FROM customers ORDER BY inv DESC LIMIT 1")
+    print(cursor.fetchone()[0], "is the draft invoice no.")
+
+def calc_gst(p,rate):
+    CGST = p*((rate/2)/100)
+    SGST = CGST
+    amount = p + CGST + SGST
+    amount = round(amount,2)
+    return CGST, SGST, amount
+
+def items():
+    inv      = int(input("Add to Invoice no: "))
+    prod     = input("Product Name: ")
+    model    = input("Model No: ")
+    serial   = input("Sr No. ")
+    qty      = int(input("Qty. "))
+    price    = float(input("Enter Price before GST: "))
+    calcrate = float(input("Rate of GST: "))
+    gst = calc_gst(price,calcrate)
+    CGST,SGST,amount = gst
+    amount*=qty
+
+    p_query = f'''insert into products (inv, prod,model,serial,qty,price,calcrate,amount) values({inv},'{prod}','{model}','{serial}',{qty},{price},{calcrate},{amount})'''
+    cursor.execute(p_query)
+    
+    connection.commit()      
+    print(cursor.rowcount, "record(s) inserted.")
+
 try:
     aborted = False
     while not aborted:
-        # inv = int(input("Invoice No: "))
-        party = input("Party Name: ")
-        date = input("Date of Invoice: ")
-        address = input("Client Address: ")
-        mobile = int(input("Client Mobile: "))
-        c_query = '''insert into customers (party,date,address,mobile) values('{}','{}','{}',{})'''.format(party,date,address,mobile)
-        cursor.execute(c_query)
-        
-        connection.commit()
-        
-        print(cursor.rowcount, "record(s) inserted.")
+        clients()
 
+        print("Proceeding towards Products Invoice...\n")
+
+        items()
         abort = input("Do you want to continue?: ")
         if abort != chr(27):
             continue
         else:
             aborted = True
-            break
             print("DB updated;\n")
-    print("Proceeding towards Products Invoice...\n")
-
-    def calc_gst(p,rate):
-        CGST = p*((rate/2)/100)
-        SGST = CGST
-        amount = p + CGST + SGST
-        return CGST, SGST, amount
-    while not aborted:
-        prod     = input("Product Name: ")
-        model    = input("Model No: ")
-        serial   = int(input("Sr No. "))
-        qty      = int(input("Qty. "))
-        price    = float(input("Enter Price before GST: "))
-        calcrate = float(input("Rate of GST: "))
-        gst = calc_gst(price,calcrate)
-        CGST,SGST,amount = gst
-
-        p_query = f'''insert into products (prod,model,serial,qty,price,calcrate,amount) values({prod},{model},{serial},{qty},{price},{calcrate},{amount})'''
-        cursor.execute(p_query)
-        
-        connection.commit()
-        
-
+            break
 
 except mysql.connector.Error as error:
     print("Failed to insert data: {}".format(error))
--- test.py
+++ test.py
@@ -1,16 +1,19 @@
-SP=float(input('Enter Selling Price(₹) : '))
-GST_Rate=float(input('Enter GST Rate : '))
+# SP=float(input('Enter Selling Price(₹) : '))
+# GST_Rate=float(input('Enter GST Rate : '))
 
-CGST=SP*((GST_Rate/2)/100)
-SGST=CGST
-A= SP+CGST+SGST
+# CGST=SP*((GST_Rate/2)/100)
+# SGST=CGST
+# A= SP+CGST+SGST
 
-print()
-print('           INVOICE')
-print('-'*25)
-print('Selling Price(₹) :',SP)
-print('CGST             :',CGST)
-print('SGST             :',SGST)
-print('Amount(₹)        :',A)
-print('-'*25)
-print()
+# print()
+# print('           INVOICE')
+# print('-'*25)
+# print('Selling Price(₹) :',SP)
+# print('CGST             :',CGST)
+# print('SGST             :',SGST)
+# print('Amount(₹)        :',A)
+# print('-'*25)
+# print()
+
+# no = float(35839.9872)
+# print(round(no,2))
